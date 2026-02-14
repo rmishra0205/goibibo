c610d3b43c9f89ceae554de845b76a5e28aba02f
Raghvendra Mishra <rmishra0205@gmail.com>
2026-02-14 07:26:32 +0530

    coderefactor

---
+++ src/main/java/com/example/goibibo/GoibiboApplication.java
@@ -0,0 +1,13 @@
+package com.example.goibibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoibiboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoibiboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/goibibo/GoibiboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goibibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoibiboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
